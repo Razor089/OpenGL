0d8040db38d48d2ec7eb77b22099ff2c704b374d
Cosimo Davide Viggiano <cosimod089@gmail.com>
2021-02-20 23:16:56 +0100

    Aggiunta dei commenti alla classe Rect

--- src/Rect.h
+++ src/Rect.h
@@ -5,23 +5,59 @@ class Rect
 {
     private:
         float _x, _y, _z;
+        int color[];
         float _speed;
         int _dimension[];
         
     public:
+        /// Crea un oggetto rettangolo
         Rect();
+
+        /// Crea un oggetto rettangolo con parametri
         Rect(float x, float y, float z, int dimension[], float speed);
+
+        /// Distruttore
         ~Rect();
+        
+        /// Imposta la posizione sull'asse X
         void SetX(float x);
+        
+        /// Imposta la posizione sull'asse Y
         void SetY(float y);
+        
+        /// Imposta la posizione sull'asse Z
         void SetZ(float z);
+
+        ///Metodo che imposta la velocità
         void SetSpeed(float speed);
+
+        /// Imposta le dimensioni del rettangolo
+        /// int w = larghezza
+        /// int h = altezza
         void SetDimension(int w, int h);
+        
+        /// Imposta il colore del rettangolo
+        /// int r = 0-255 rosso
+        /// int g = 0-255 verde
+        /// int b = 0-255 blue
+        void SetColor(int r, int g, int b);
+        
+        /// Metodo che esegue tutti i calcoli di movimento e rotazione
         void Calculate();
+        
+        /// Metodo che disegna il rettangolo a schermo
         void Draw();
+        
+        /// Ritorna la posizione sull'asse X
         float GetX();
+        
+        /// Ritorna la posizione sull'asse Y
         float GetY();
+        
+        /// Ritorna la posizione sull'asse Z
         float GetZ();
+        
+        /// Ritorna la velocità
         float GetSpeed();
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,17 @@
 
 using namespace std;
 
+// Prototipi
+
 void display();
 void reshape(int w, int h);
 void init();
 
 //Objects
-Rect *player;
 
+Rect *player, *ai;
+
+/// Main
 int main(int args, char** argv)
 {
     glutInit(&args, argv);
@@ -33,6 +37,7 @@ int main(int args, char** argv)
     return 0;
 }
 
+/// Inizializzazione delle variabili e dello stato di OpenGL
 void init()
 {   
     glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -43,8 +48,10 @@ void init()
     dim[0] = 20;
     dim[1] = 5;
     player = new Rect(0, -99, 0, dim, 0);
+    ai = new Rect(0, 99, 0, dim, 0);
 }
 
+/// Metodo che governa il ridimensionamento della finestra
 void reshape(int w, int h)
 {
     glViewport(0, 0, (GLsizei)600, (GLsizei)600);
@@ -55,15 +62,18 @@ void reshape(int w, int h)
     glLoadIdentity();
 }
 
+/// Metodo dove si disegna tutto sullo schermo
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    ai->Draw();
     player->Draw();
 
     glutSwapBuffers();
 }
 
+/// Metodo che permette di stampare a schermo
 void printw(float x, float y, float z, char* format, ...)
 {
     
